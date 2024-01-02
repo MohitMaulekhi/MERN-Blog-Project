846d217e51e4e07754c7d283dcc41c0080eded19
MohitMaulekhi <mohitmaulekhi312@gmail.com>
2024-01-02 19:19:22 +0530

    Changes to be committed: 	modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   backend/src/app.js 	modified:   backend/src/index.js 	new file:   backend/src/utils/ApiError.js 	new file:   backend/src/utils/ApiResponse.js 	new file:   backend/src/utils/asyncHandler.js

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+
+
+// Configuration
+app.use(express.json({  //limiting json data
+    limit:"256kb"
+}))
+app.use(express.urlencoded({   // making enconding standard and limiting it
+    extended:true,
+    limit: "256kb"
+}))
+app.use(express.static("public")) // serving public folder as static for app
+app.use(cookieParser()) // for using secure cookies
+
+
+
+export {app};
