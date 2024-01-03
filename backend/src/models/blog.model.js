922ca5f815b7a35946c65d1f42b86ac4f92d8361
MohitMaulekhi <mohitmaulekhi312@gmail.com>
2024-01-03 19:42:51 +0530

    Changes to be committed: 	deleted:    README.md 	modified:   backend/package.json 	new file:   backend/src/middlewares/multer.middleware.js 	new file:   backend/src/models/blog.model.js 	new file:   backend/src/models/user.model.js 	new file:   backend/src/utils/cloudinary.js

---
+++ backend/src/models/blog.model.js
@@ -0,0 +1,18 @@
+import mongoose,{Schema} from "mongoose"
+const blogSchema = new Schema(
+    {
+        heading:{
+            type: String,
+            required: true,
+            index:true
+        },
+        img:{
+            type:String
+        },
+        content:{
+            type:String
+        }
+    }
+,{timestamps:true})
+
+export const Blog = mongoose.model("Blog",blogSchema)
